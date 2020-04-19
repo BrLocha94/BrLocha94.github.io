1b4c1b99facb93acf7e738a87c2957f15dde25f1
BrLocha94 <brunolocha2@gmail.com>
2020-04-19 17:00:27 -0300

    Updates

---
+++ precache-manifest.149093f97ca27da6e7d3efdefd6f2772.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f6cb31a5cc5fceb0af9b9f67f575896",
+    "url": "/portifolio/index.html"
+  },
+  {
+    "revision": "fc1ab05af84d4175637d",
+    "url": "/portifolio/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "9a241f36eb5b9f6bda20",
+    "url": "/portifolio/static/js/2.1220af68.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portifolio/static/js/2.1220af68.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc1ab05af84d4175637d",
+    "url": "/portifolio/static/js/main.08a9e176.chunk.js"
+  },
+  {
+    "revision": "4bb4e00399b5822850be",
+    "url": "/portifolio/static/js/runtime-main.1f21d04c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portifolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
